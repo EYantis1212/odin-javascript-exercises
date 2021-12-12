fcbca6cdbdfd755c8e42166d74d8f0a5e9dde463
Yantis <yantis1212@outlook.com>
2021-12-12 11:28:10 -0700

    Exercise 3 Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function(str) {
-
+const reverseString = function (str) {
+    let splitStr = str.split("");
+    let reverseArr = splitStr.reverse();
+    let joinArr = reverseArr.join("");
+    return joinArr;
 };
-
+// console.log(reverseString("Hello"));
 // Do not edit below this line
 module.exports = reverseString;
