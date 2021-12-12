cf2f1925cee5dcd118639ccd2b531f56eb0ac9ea
Yantis <yantis1212@outlook.com>
2021-12-12 16:43:57 -0700

    Exercise 4 Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...removedEle) {
+    let newArr = arr.filter((item) => !removedEle.includes(item));
+    return newArr;
 };
-
+console.log(removeFromArray([1, 2, 3, 4], 2, 3));
 // Do not edit below this line
-module.exports = removeFromArray;
+// module.exports = removeFromArray;
