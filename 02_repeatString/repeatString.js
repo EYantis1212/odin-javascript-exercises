4a8f275151f219116781623ccc76e7cc1dfe1fb0
Yantis <yantis1212@outlook.com>
2021-12-12 10:26:21 -0700

    Exercise 2 Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num >= 0) {
+        let str = "";
+        for (let i = 0; i < num; i++) {
+            str = str.concat(string);
+        }
+        return str;
+    } else return "ERROR";
 };
-
+// repeatString("hey", 3);
+// repeatString("hey", 3);
+// repeatString("hey", 3);
 // Do not edit below this line
 module.exports = repeatString;
