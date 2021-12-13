25b67bf2a288e29e777600af3ec3aceecb528a53
Yantis <yantis1212@outlook.com>
2021-12-13 09:51:51 -0700

    Exercise 5 Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    let sum = 0;
+    if (typeof x === "number" && typeof y === "number" && y >= 0 && x >= 0) {
+        if (x < y) {
+            for (let i = x; i <= y; i++) {
+                sum += i;
+            }
+            return sum;
+        } else if (x > y) {
+            for (let i = y; i <= x; i++) {
+                sum += i;
+            }
+            return sum;
+        }
+    } else return "ERROR";
 };
 
 // Do not edit below this line
