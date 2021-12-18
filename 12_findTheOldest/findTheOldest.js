1ec698fcb93df69a3c9b13e56a23ff4fb65f8446
Yantis <yantis1212@outlook.com>
2021-12-18 16:44:38 -0700

    Challenge 12_findTheOldest Complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,40 @@
-const findTheOldest = function() {
+const people = [
+	{
+		name: "Carly",
+		yearOfBirth: 1942,
+		yearOfDeath: 1970,
+	},
+	{
+		name: "Ray",
+		yearOfBirth: 1962,
+		yearOfDeath: 2011,
+	},
+	{
+		name: "Jane",
+		yearOfBirth: 1912,
+		yearOfDeath: 1941,
+	},
+];
 
+const findTheOldest = function (array) {
+	return array.reduce((previousPerson, currentPerson) => {
+		const previousAge = findAge(
+			previousPerson.yearOfDeath,
+			previousPerson.yearOfBirth
+		);
+		const currentAge = findAge(
+			currentPerson.yearOfDeath,
+			currentPerson.yearOfBirth
+		);
+		return previousAge < currentAge ? currentPerson : previousPerson;
+	});
 };
+function findAge(death, birth) {
+	if (!death) {
+		death = new Date().getFullYear();
+	}
+	return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
