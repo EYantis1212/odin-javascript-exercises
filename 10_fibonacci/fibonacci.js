332c2310ed562577439e7deb2ae820014a7b1508
Yantis <yantis1212@outlook.com>
2021-12-18 15:34:13 -0700

    Challenge 10_fibonacci Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let n1 = 0;
+    let n2 = 1;
+    if (num > 0) {
+        for (let i = 0; i < num; i++) {
+            next = n1 + n2;
+            n1 = n2;
+            n2 = next;
+        }
+        return n1;
+    } else return "OOPS";
 };
 
 // Do not edit below this line
