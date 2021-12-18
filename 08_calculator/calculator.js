fe78bfa4fd9093007facf193ff07ba014eb47eae
Yantis <yantis1212@outlook.com>
2021-12-18 13:23:12 -0700

    Challenge 08 (Calculator) Complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+    let arrSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        arrSum += arr[i];
+    }
+    return arrSum;
 };
 
-const multiply = function() {
-
+const power = function (a, b) {
+    return a ** b;
 };
 
-const power = function() {
-	
+const multiply = function (arr) {
+    let product = 1;
+    for (let i = 0; i < arr.length; i++) {
+        product = product * arr[i];
+    }
+    return product;
 };
-
-const factorial = function() {
-	
+console.log(multiply([2, 3, 4, 5, 6, 7, 8]));
+const factorial = function (num) {
+    let f = 1;
+    for (i = num; i >= 1; i--) {
+        f = f * i;
+    }
+    return f;
 };
-
+console.log(factorial(10));
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
