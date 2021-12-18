3a866ba5c01d0c93e0a2c9296987559bcd25e442
Yantis <yantis1212@outlook.com>
2021-12-18 15:03:39 -0700

    Challenge 09_palindromes Complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let string = str
+        .split("")
+        .join("")
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+    let splitReverse = str
+        .split("")
+        .reverse()
+        .join("")
+        .toLowerCase()
+        .replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+    console.log(string);
+    console.log(splitReverse);
+    if (string === splitReverse) {
+        return true;
+    } else return false;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
